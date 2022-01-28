e3722ff8e5a1e83be68f0c294451d9f90a1f172f
Ilnytska <olly.happyfly@gmail.com>
2022-01-28 22:59:41 +0200

    Update index.js
    add import renderfunction

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,21 @@
 import '../sass/main.scss';
 import './modal-window/index';
 import fetch from './fetch';
-// import movieCard from '../handlebars/movie-card.hbs'
+import renderMarkupMovieCard from './movie-card'
 let page = 2;
-// const gallery = document.querySelector('.movie-card__list');
+const gallery = document.querySelector('.movie-card__list');
 const fetchPopularMoviesList = () => {
   fetch(page)
     .then(res => {
       if (res.results.length === 0) {
         console.log('null result');
       }
-      res.results.forEach(movie => {
-        console.log(movie.title, movie.id);
-      });
-      // renderMarkupMovieCard(res);
+      // res.results.forEach(movie => {
+      //   console.log(movie.title, movie.id);
+      // });
+      renderMarkupMovieCard(res);
     })
     .catch(err => console.log(err));
 };
 
 fetchPopularMoviesList(page);
-
-
-// function renderMarkupMovieCard(data) {
-//   gallery.insertAdjacentHTML('beforeend', movieCard(data));
-// }
