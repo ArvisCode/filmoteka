41dc79dc82bc8ad06e16b800fae5bf17d24cbd92
AKdimka <88268598+AKdimka@users.noreply.github.com>
2022-01-29 12:29:07 +0200

    fix all bugs

--- src/js/header.js
+++ src/js/header.js
@@ -18,25 +18,34 @@ refs.watchedBtn.addEventListener('click', onWatchedBtnClick);
 refs.queueBtn.addEventListener('click', onQueueBtnClick);
 
 function onLibraryBtnClick() {
+	headerRender()
+}
+function onHomeBtnClick() {
+	headerRender()
+}
+function headerRender() {
 	if (refs.header.classList.contains('header--home')) {
+		refs.libraryBtn.setAttribute('disabled', true);
+		refs.logo.removeAttribute('disabled');
+		refs.homeBtn.removeAttribute('disabled');
 		refs.header.classList.toggle('header--library');
+		refs.header.classList.toggle('header--home');
 		refs.homeBtn.classList.toggle('navigation__btn--current');
 		refs.libraryBtn.classList.toggle('navigation__btn--current');
 		refs.searchInput.classList.toggle('on-library-none');
-		refs.libraryBtnsContainer.classList.toggle('on-home-none')
-	}
-}
-
-function onHomeBtnClick() {
-	if (refs.header.classList.contains('header--library')) {
+		refs.libraryBtnsContainer.classList.toggle('on-home-none');
+	} else {
+		refs.homeBtn.setAttribute('disabled', true);
+		refs.logo.setAttribute('disabled', true);
+		refs.libraryBtn.removeAttribute('disabled');
 		refs.header.classList.toggle('header--library');
+		refs.header.classList.toggle('header--home');
 		refs.libraryBtn.classList.toggle('navigation__btn--current');
 		refs.homeBtn.classList.toggle('navigation__btn--current');
 		refs.searchInput.classList.toggle('on-library-none');
 		refs.libraryBtnsContainer.classList.toggle('on-home-none');
 	}
 }
-
 function onWatchedBtnClick() {
 	if (refs.queueBtn.classList.contains('library-btn--current')) {
 		refs.watchedBtn.classList.toggle('library-btn--current')
