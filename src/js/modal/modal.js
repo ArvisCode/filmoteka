f5983ff8defe993a4118306942624f4bb84a460e
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-01-30 20:19:22 +0100

    update modal for work with tag LI

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -7,16 +7,18 @@ const movieList = document.querySelector('.movie-card__list');
 movieList.addEventListener('click', onClickList);
 
 function onClickList(e) {
-  e.preventDefault();
-  const currentId = e.target.closest('.movie-card__link').id;
+  const currentId = e.target.closest('.movie-card__item').dataset.id;
 
   fetchById(currentId)
     .then(data => {
       const modal = basicLightbox.create(previewTemplate(data));
-      console.log(data);
       const closeBtn = modal.element().querySelector('[data-close]');
       closeBtn.addEventListener('click', () => modal.close());
       modal.show();
+
+      if (modal.visible) {
+        window.addEventListener('keydown', onPressKeyESC);
+      }
 
       function onPressKeyESC(e) {
         if (e.code === 'Escape') {
@@ -24,10 +26,6 @@ function onClickList(e) {
           window.removeEventListener('keydown', onPressKeyESC);
         }
       }
-
-      if (modal.visible) {
-        window.addEventListener('keydown', onPressKeyESC);
-      }
     })
-    .catch(console.log(error));
+    .catch(error => console.log(error));
 }
