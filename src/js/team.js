f5bf12bf3a7d43164fc1775fef5646e89a746f00
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2022-01-30 23:35:34 +0200

    add team

---
+++ src/js/team.js
@@ -0,0 +1,45 @@
+const team = {
+  group: 'project-group-5',
+  teamName: 'A.C.C.O.R.D.',
+  teamNameDecoding: 'alternative code command of real development',
+  teamLead: {
+    name: 'Volodymyr Horbatiuk',
+    link: 'https://github.com/ArvisCode',
+  },
+  scrumMaster: {
+    name: 'Olesia Lykhovyd',
+    link: 'https://github.com/OlesiaLykhovyd',
+  },
+  developers: [
+    {
+      name: 'Bohdan Strilets',
+      link: 'https://github.com/Boguszz',
+    },
+    {
+      name: 'Vadim Dudkevich',
+      link: 'https://github.com/Vadim-Dudkevich',
+    },
+    {
+      name: 'Denys Pitel',
+      link: 'https://github.com/DenysPitel',
+    },
+    {
+      name: 'Olha Ilnytska',
+      link: 'https://github.com/Ilnytska',
+    },
+    {
+      name: 'Nadia Eismont',
+      link: 'https://github.com/NadiaEismont',
+    },
+    {
+      name: 'Dmitriy Brigadir',
+      link: 'https://github.com/AKdimka',
+    },
+    {
+      name: 'Oleksandr Vaskevych',
+      link: 'https://github.com/Oleksandr-full-stack-development',
+    },
+  ],
+};
+
+export { team };
