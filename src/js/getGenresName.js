8fbf1fb2bba21dd21e13246b882f7837140f8528
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2022-01-30 22:07:34 +0200

    fix request conflict-bug

--- src/js/getGenresName.js
+++ src/js/getGenresName.js
@@ -1,16 +1,17 @@
-export const getGenresNames = function (genreIds) {
+import genres from './DATA/genres';
 
-    const genresNames = [];
-    for (let genreId of genreIds) {
-        genres.genres.forEach((genre) => {
-            if (genreId === genre.id) {
-                genresNames.push(genre.name);
-            }
-        });
-    }
-    const genre2 = genresNames.slice(0, 2);
-    if (genresNames.length > 2) {
-        genre2.push('Others');
-    }
-    return genre2.join(', ');
-}
+export const getGenresNames = function (genreIds) {
+  const genresNames = [];
+  for (let genreId of genreIds) {
+    genres.genres.forEach(genre => {
+      if (genreId === genre.id) {
+        genresNames.push(genre.name);
+      }
+    });
+  }
+  const genre2 = genresNames.slice(0, 2);
+  if (genresNames.length > 2) {
+    genre2.push('Others');
+  }
+  return genre2.join(', ');
+};
--- src/js/index.js
+++ src/js/index.js
@@ -4,31 +4,32 @@ import fetch from './fetch';
 import header from './header';
 import Pagination from 'tui-pagination';
 
-import genres from './DATA/genres';
 import { getGenresNames } from './getGenresName';
-import scrollTop from './scrollToTop'
+import scrollTop from './scrollToTop';
 import './input-search';
 import { spinner, startSpinner, hideLoader } from './spinner';
-// import movieCard from '../handlebars/movie-card.hbs'
+import movieCard from '../handlebars/movie-card.hbs';
 
 import renderMarkupMovieCard from './movie-card';
+
 let page = 2;
+
 const gallery = document.querySelector('.movie-card__list');
+
 const fetchPopularMoviesList = () => {
-	fetch(page)
-		.then(res => {
-			if (res.results.length === 0) {
-				console.log('null result');
-			}
-			// res.results.forEach(movie => {
-			//   console.log(movie.title, movie.id);
-			// });
-			res.results.forEach(movie => {
-				movie.genre_ids = getGenresNames(movie.genre_ids);
-			});
-			renderMarkupMovieCard(res);
-		})
-		.catch(err => console.log(err));
+  fetch(page)
+    .then(res => {
+      if (res.results.length === 0) {
+        console.log('null result');
+      }
+
+      res.results.forEach(movie => {
+        movie.genre_ids = getGenresNames(movie.genre_ids);
+      });
+
+      renderMarkupMovieCard(res);
+    })
+    .catch(err => console.log(err));
 };
 
 fetchPopularMoviesList(page);
@@ -37,9 +38,6 @@ fetchPopularMoviesList(page);
 // const options = {
 // };
 // const pagination = new Pagination(container, options);
-
-
-
 
 // function renderMarkupMovieCard(data) {
 //   gallery.insertAdjacentHTML('beforeend', movieCard(data));
