a6406916e28992492f41413034cabd1ca1a16f74
Nadia Nechuiviter <nadiaejsmont@gmail.com>
2022-01-31 21:30:34 +0200

    watched

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,13 +1,20 @@
 // import inputSearch from './input-search';
 const spinner = document.querySelector('.loadingSpinner');
+const backdrop = document.querySelector('.backdrop');
 
 async function startSpinner() {
     spinner.style.display = "block";
-    await new Promise(r => setTimeout(r, 3000));
+    toggleModal();
+    await new Promise(r => setTimeout(r, 1000));
 }
 
 function hideLoader() {
+    toggleModal()
     spinner.style.display = "none";
 }
+function toggleModal() {
+    backdrop.classList.toggle('is-hidden');
+}
 
-module.exports = { spinner, startSpinner, hideLoader };
+
+module.exports = { spinner, startSpinner, hideLoader, toggleModal };
