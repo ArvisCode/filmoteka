9fbfbc9123eee29f711c5e7e3b0d8c8b522fb6e5
Ilnytska <olly.happyfly@gmail.com>
2022-01-31 22:10:17 +0200

    Update movie-card.js
    change title, poster-size

--- src/js/movie-card.js
+++ src/js/movie-card.js
@@ -1,22 +1,21 @@
 export default function renderMarkupMovieCard({ results }) {
     const gallery = document.querySelector('.movie-card__list')
   const markup = results
-    .map(({ id, poster_path, original_title, genre_ids, original_language, vote_average, title, release_date }) => {
+    .map(({ id, poster_path, genre_ids, original_language, vote_average, title, release_date }) => {
       return `
       <li class="movie-card__item" data-id="${id}">
                 <div class="movie-card">
                  ${
                   poster_path
-                    ? `<img src="https://image.tmdb.org/t/p/w342${poster_path}"`
+                    ? `<img src="https://image.tmdb.org/t/p/w500${poster_path}"`
                     : `<img src="https://yt3.ggpht.com/AAKF_677TIvjFz_9xFF0R6PgiVd0kRpEtY6APSxSDRP65nXg8hkn9NFsz2bRd9_Z37DJ9D_b=s900-c-k-c0x00ffffff-no-rj"`
                 //  :`<img src="https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg"`
                  }
-                        class="movie-card__poster"width="305"
-                        height="205"
+                        class="movie-card__poster"
                         alt="${title}"
                         loading="lazy"
                     />
-                    <h2 class="movie-info-title"> ${original_title}</h2>
+                    <h2 class="movie-info-title"> ${title}</h2>
                     <div class="movie-card__thumb">
                     <div class="movie-info-list">
                         <p class="info-item"> ${genre_ids}</p>
