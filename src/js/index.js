8fd98d41875c97f4d555e32a10ebdd8ff88fb455
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2022-02-01 15:06:12 +0200

    test

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,15 @@ import '../sass/main.scss';
 import './modal/modal';
 import fetch from './fetches/fetch';
 import header from './header';
-import Pagination from 'tui-pagination';
-
 import { getGenresNames } from './getGenresName';
 import scrollTop from './scrollToTop';
 import './input-search/input-search';
 import { spinner, startSpinner, hideLoader } from './spinner';
 import movieCard from '../handlebars/movie-card.hbs';
-
 import renderMarkupMovieCard from './movie-card';
 import { team } from './team';
+
+import Pagination from 'tui-pagination';
 
 let page = 1;
 
@@ -20,9 +19,6 @@ const gallery = document.querySelector('.movie-card__list');
 const fetchPopularMoviesList = () => {
   fetch(page)
     .then(res => {
-      if (res.results.length === 0) {
-        console.log('null result');
-      }
       res.results.forEach(movie => {
         movie.genre_ids = getGenresNames(movie.genre_ids);
       });
@@ -31,15 +27,16 @@ const fetchPopularMoviesList = () => {
     })
     .catch(err => console.log(err));
 };
+export { fetchPopularMoviesList };
 
 fetchPopularMoviesList(page);
-export { fetchPopularMoviesList };
-// const container = document.querySelector('tui-pagination');
-// const options = {
-// };
-// const pagination = new Pagination(container, options);
 
-// function renderMarkupMovieCard(data) {
-//   gallery.insertAdjacentHTML('beforeend', movieCard(data));
-// }
+//const container = document.querySelector('tui-pagination');
+//const options = {};
+//const pagination = new Pagination(container, options);
+
+//(function renderMarkupMovieCard(data) {
+//  gallery.insertAdjacentHTML('beforeend', movieCard(data));
+//})();
+console.log('About developers');
 console.log(team);
