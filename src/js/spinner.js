1b612d9afaad608f379467cf7d2067a887611be0
Volodymyr Horbatiuk <vladimir.a.gorbatyuk@gmail.com>
2022-02-01 18:02:03 +0200

    delete timeout

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,20 +1,18 @@
-// import inputSearch from './input-search';
 const spinner = document.querySelector('.loadingSpinner');
 const backdrop = document.querySelector('.backdrop');
 
 async function startSpinner() {
-    spinner.style.display = "block";
-    toggleModal();
-    await new Promise(r => setTimeout(r, 1000));
+  spinner.style.display = 'block';
+  toggleModal();
 }
 
 function hideLoader() {
-    toggleModal()
-    spinner.style.display = "none";
-}
-function toggleModal() {
-    backdrop.classList.toggle('is-hidden');
+  toggleModal();
+  spinner.style.display = 'none';
 }
 
+function toggleModal() {
+  backdrop.classList.toggle('is-hidden');
+}
 
 module.exports = { spinner, startSpinner, hideLoader, toggleModal };
