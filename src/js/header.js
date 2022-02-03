8df33734e2881289aecf8f85d03b63f07ccb538b
AKdimka <88268598+AKdimka@users.noreply.github.com>
2022-02-03 19:56:05 +0200

    add and fix styles

--- src/js/header.js
+++ src/js/header.js
@@ -3,15 +3,15 @@ import { renderPopularMoviesList } from './fetches/renderPopularMovieList';
 import { myLibraryPage } from './myLibraryPage';
 
 const refs = {
-  header: document.querySelector('.header'),
-  logo: document.querySelector('.logo'),
-  homeBtn: document.querySelector('#home-btn'),
-  libraryBtn: document.querySelector('#library-btn'),
-  searchInput: document.querySelector('.search-field'),
-  libraryBtnsContainer: document.querySelector('.library-btns-container'),
-  watchedBtn: document.querySelector('#watched-btn'),
-  queueBtn: document.querySelector('#queue-btn'),
-  movieCardList:document.querySelector('.movie-card__list'),
+	header: document.querySelector('.header'),
+	logo: document.querySelector('.logo'),
+	homeBtn: document.querySelector('#home-btn'),
+	libraryBtn: document.querySelector('#library-btn'),
+	searchInput: document.querySelector('.search-field'),
+	libraryBtnsContainer: document.querySelector('.library-btns-container'),
+	watchedBtn: document.querySelector('#watched-btn'),
+	queueBtn: document.querySelector('#queue-btn'),
+	movieCardList: document.querySelector('.movie-card__list'),
 };
 
 refs.libraryBtn.addEventListener('click', onLibraryBtnClick);
@@ -21,46 +21,54 @@ refs.watchedBtn.addEventListener('click', onWatchedBtnClick);
 refs.queueBtn.addEventListener('click', onQueueBtnClick);
 
 function onLibraryBtnClick() {
-  headerRender();
-  myLibraryPage();
+	createHeaderLibrary();
+	myLibraryPage();
 }
+
 function onHomeBtnClick() {
-  headerRender();
-  refs.movieCardList.classList.remove('library');
-  renderPopularMoviesList();
+	renderHeaderHome();
+	refs.movieCardList.classList.remove('library');
+	renderPopularMoviesList();
 }
-function headerRender() {
-  if (refs.header.classList.contains('header--home')) {
-    refs.libraryBtn.setAttribute('disabled', true);
-    refs.logo.removeAttribute('disabled');
-    refs.homeBtn.removeAttribute('disabled');
-    refs.header.classList.toggle('header--library');
-    refs.header.classList.toggle('header--home');
-    refs.homeBtn.classList.toggle('navigation__btn--current');
-    refs.libraryBtn.classList.toggle('navigation__btn--current');
-    refs.searchInput.classList.toggle('on-library-none');
-    refs.libraryBtnsContainer.classList.toggle('on-home-none');
-  } else {
-    refs.homeBtn.setAttribute('disabled', true);
-    refs.logo.setAttribute('disabled', true);
-    refs.libraryBtn.removeAttribute('disabled');
-    refs.header.classList.toggle('header--library');
-    refs.header.classList.toggle('header--home');
-    refs.libraryBtn.classList.toggle('navigation__btn--current');
-    refs.homeBtn.classList.toggle('navigation__btn--current');
-    refs.searchInput.classList.toggle('on-library-none');
-    refs.libraryBtnsContainer.classList.toggle('on-home-none');
-  }
+
+function renderHeaderHome() {
+	if (refs.header.classList.contains('header--library')) {
+		refs.homeBtn.setAttribute('disabled', true);
+		refs.logo.setAttribute('disabled', true);
+		refs.libraryBtn.removeAttribute('disabled');
+		refs.header.classList.toggle('header--library');
+		refs.header.classList.toggle('header--home');
+		refs.libraryBtn.classList.toggle('navigation__btn--current');
+		refs.homeBtn.classList.toggle('navigation__btn--current');
+		refs.searchInput.classList.toggle('on-library-none');
+		refs.libraryBtnsContainer.classList.toggle('on-home-none');
+	}
 }
+
+function createHeaderLibrary() {
+	if (refs.header.classList.contains('header--home')) {
+		refs.libraryBtn.setAttribute('disabled', true);
+		refs.logo.removeAttribute('disabled');
+		refs.homeBtn.removeAttribute('disabled');
+		refs.header.classList.toggle('header--library');
+		refs.header.classList.toggle('header--home');
+		refs.homeBtn.classList.toggle('navigation__btn--current');
+		refs.libraryBtn.classList.toggle('navigation__btn--current');
+		refs.searchInput.classList.toggle('on-library-none');
+		refs.libraryBtnsContainer.classList.toggle('on-home-none');
+	}
+}
+
 function onWatchedBtnClick() {
-  if (refs.queueBtn.classList.contains('library-btn--current')) {
-    refs.watchedBtn.classList.toggle('library-btn--current');
-    refs.queueBtn.classList.toggle('library-btn--current');
-  }
+	if (refs.queueBtn.classList.contains('library-btn--current')) {
+		refs.watchedBtn.classList.toggle('library-btn--current');
+		refs.queueBtn.classList.toggle('library-btn--current');
+	}
 }
+
 function onQueueBtnClick() {
-  if (refs.watchedBtn.classList.contains('library-btn--current')) {
-    refs.queueBtn.classList.toggle('library-btn--current');
-    refs.watchedBtn.classList.toggle('library-btn--current');
-  }
+	if (refs.watchedBtn.classList.contains('library-btn--current')) {
+		refs.queueBtn.classList.toggle('library-btn--current');
+		refs.watchedBtn.classList.toggle('library-btn--current');
+	}
 }
