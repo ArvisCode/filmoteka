06f87613dad5e37713a0c838fc6a441f070dcef2
Oleksandr Vaskevych <oleksandrvaskevich@gmail.com>
2022-02-04 17:39:02 +0200

    make_constant_LOCAL_STORAGE_WATCHED_and_QUEUE

--- src/js/getFromLocalStorageWatchedList.js
+++ src/js/getFromLocalStorageWatchedList.js
@@ -2,6 +2,8 @@
 import renderMarkupMovieCard from './movie-card.js';
 import { getGenresNames } from './getGenresName';
 import genres from './DATA/genres';
+import imagePing from 'tui-pagination';
+import { LOCAL_STORAGE_WATCHED } from './buttonWatched.js';
 
 const watchedButton = document.querySelector('#watched-btn');
 const gallery = document.querySelector('.movie-card__list');
@@ -9,7 +11,7 @@ const gallery = document.querySelector('.movie-card__list');
 watchedButton.addEventListener('click',getWatchedFilms);
 
 export default function getWatchedFilms () {
-    const getFilms = localStorage.getItem('movies');
+    const getFilms = localStorage.getItem(LOCAL_STORAGE_WATCHED);
     const data = JSON.parse(getFilms);
     gallery.innerHTML = '';
     console.log(data);
--- src/js/getfromLocalStorageQueueList.js
+++ src/js/getfromLocalStorageQueueList.js
@@ -1,6 +1,8 @@
 import { getGenresNames } from '../js/getGenresName';
 import genres from '../js/DATA/genres';
 import renderMarkupMovieCard from './movie-card.js';
+import imagePing from 'tui-pagination';
+import { LOCAL_STORAGE_QUEUE } from './buttonWatched';
 
 const queueButton = document.querySelector('#queue-btn');
 const gallery = document.querySelector('.movie-card__list');
@@ -8,7 +10,7 @@ const gallery = document.querySelector('.movie-card__list');
 queueButton.addEventListener('click',getQueueFilms);
 
 export default function getQueueFilms () {
-    const getFilms = localStorage.getItem('queueMovie');
+    const getFilms = localStorage.getItem(LOCAL_STORAGE_QUEUE);
     const data = JSON.parse(getFilms);
     gallery.innerHTML = '';
     console.log(data);
