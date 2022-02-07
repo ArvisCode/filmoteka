94fe3366542c9b3faa094678845a86a97afdd2b7
Ilnytska <olly.happyfly@gmail.com>
2022-02-07 16:48:42 +0200

    Update switcher.js

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -1,7 +1,4 @@
-// function setTheme(themeName) {
-//         localStorage.setItem('theme', themeName);
-//         document.documentElement.className = themeName;
-//       }
+
 //       function toggleTheme() {
 //         if (localStorage.getItem('theme') === 'theme-dark') {
 //           setTheme('theme-light');
@@ -18,3 +15,19 @@
 //           document.getElementById('slider').checked = true;
 //         }
 //       })();
+const input = document.querySelector(".switcher-toggle");
+
+// Отслеживаем щелчок по кнопке
+input.addEventListener("click", function toggleTheme() {
+    // Затем переключаем (добавляем/удаляем) класс .dark-theme для body
+    document.body.classList.toggle("theme-dark");
+    (function () {
+        if (document.body.classList.toggle("theme-dark")) {
+            document.getElementById('slider').checked = false;
+        } else {
+          
+            document.getElementById('slider').checked = true;
+        }
+    
+    });
+})
