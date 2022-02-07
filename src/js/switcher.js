fce146bdd5440015ac6ea0e92fb87948f3a77cac
Ilnytska <olly.happyfly@gmail.com>
2022-02-07 14:39:29 +0200

    Create switcher.js
    create file switcher.js

---
+++ src/js/switcher.js
@@ -0,0 +1,20 @@
+function setTheme(themeName) {
+        localStorage.setItem('theme', themeName);
+        document.documentElement.className = themeName;
+      }
+      function toggleTheme() {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+          setTheme('theme-light');
+        } else {
+          setTheme('theme-dark');
+        }
+      }
+      (function () {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+          setTheme('theme-dark');
+          document.getElementById('slider').checked = false;
+        } else {
+          setTheme('theme-light');
+          document.getElementById('slider').checked = true;
+        }
+      })();
